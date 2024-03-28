e84702aeb05b43180f955c841b5f3047e4f55dbe
cihanbagriyanik <cihanbagriyanikde@gmail.com>
2024-03-28 14:57:41 +0100

    home resposive desing updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,8 +31,23 @@ const Home = () => {
             paddingTop: "3rem",
           }}
         >
-          <Box>
-            <Typography fontFamily={"girassol"} fontSize={25}>
+          <Box
+            sx={{
+              "@media (max-width:500px)": {
+                fontSize: "75%",
+                marginLeft: "-1.5rem",
+              },
+            }}
+          >
+            <Typography
+              fontFamily={"girassol"}
+              fontSize={25}
+              sx={{
+                "@media (max-width:500px)": {
+                  fontSize: "1rem",
+                },
+              }}
+            >
               <span
                 style={{
                   borderBottom: "2px solid black",
@@ -44,7 +59,17 @@ const Home = () => {
               my name is
             </Typography>
 
-            <Typography fontFamily={"girassol"} variant="h3" component={"h1"}>
+            <Typography
+              fontFamily={"girassol"}
+              variant="h3"
+              component={"h1"}
+              sx={{
+                "@media (max-width:500px)": {
+                  fontSize: "1.5rem",
+                  marginTop: "0.5rem",
+                },
+              }}
+            >
               Cihan Bagriyanik
             </Typography>
             <Typography
@@ -52,7 +77,13 @@ const Home = () => {
               variant="h4"
               component={"h1"}
               color={"grey"}
-              sx={{ fontStyle: "italic" }}
+              sx={{
+                fontStyle: "italic",
+                "@media (max-width:500px)": {
+                  fontSize: "1.5rem",
+                  // marginTop: "0.5rem",
+                },
+              }}
             >
               Web Developer
             </Typography>
@@ -62,6 +93,11 @@ const Home = () => {
                 display: "flex",
                 flexDirection: "column",
                 marginTop: "3.5rem",
+
+                "@media (max-width:500px)": {
+                  fontSize: "0.7rem",
+                  marginTop: "1rem",
+                },
               }}
               gap={1}
             >
@@ -84,7 +120,9 @@ const Home = () => {
                 width: 200,
                 height: 200,
                 "@media (max-width:500px)": {
-                  display: "none",
+                  width: 100,
+                  height: 100,
+                  marginLeft: "-1.5rem",
                 },
               }}
             />
